d129a878a0f2d73f0ef7b7289c5b589a4992180f
ArthurRod <arthur9713@hotmail.com>
2022-04-07 21:49:17 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+import * as C from "./App.styles";
+import { items } from "./data/items";
+import { categories } from "./data/categories";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
+
+import { TableArea } from "./components/TableArea";
+
+function App() {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema financeiro</C.HeaderText>
+      </C.Header>
+      <C.Main>
+        <TableArea list={filteredList} />
+      </C.Main>
+    </C.Container>
+  );
+}
+
+export default App;
---
+++ src/data/items.ts
@@ -0,0 +1,41 @@
+import { Item } from "../types/Item";
+
+/* Dados fixos */
+export const items: Item[] = [
+  {
+    date: new Date(2022, 3, 5),
+    category: "salary",
+    title: "Salário",
+    value: 4500,
+  },
+  {
+    date: new Date(2022, 3, 7),
+    category: "food",
+    title: "McDonald's",
+    value: 32.12,
+  },
+  {
+    date: new Date(2022, 3, 7),
+    category: "food",
+    title: "Burguer King",
+    value: 29.8,
+  },
+  {
+    date: new Date(2022, 3, 10),
+    category: "rent",
+    title: "Aluguel",
+    value: 2500,
+  },
+  {
+    date: new Date(2022, 4, 5),
+    category: "salary",
+    title: "Salário",
+    value: 4500,
+  },
+  {
+    date: new Date(2022, 4, 10),
+    category: "rent",
+    title: "Aluguel",
+    value: 2500,
+  },
+];
